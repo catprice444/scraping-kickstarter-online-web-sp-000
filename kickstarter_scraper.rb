d2659fced390d1fec1f2196735851a5b3abca669
Catherine Price <catprice444@gmail.com>
2020-06-10 16:01:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,5 @@
-require "nokogiri"
-require "pry"
-
+require 'nokogiri'
+require 'pry'
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
@@ -15,9 +14,14 @@ def create_project_hash
 
   projects = {}
 
-  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
   end
 
   # return the projects hash
